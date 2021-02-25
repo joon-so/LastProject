d2c50e7c75a9e000c5dbde67137cc9b27d75d7bc
Lee Mi Reuk <sslejds@naver.com>
2021-02-25 22:09:04 +0900

    Add Character Jump

--- LastProject/Assets/Scripts/MainCharacter.cs
+++ LastProject/Assets/Scripts/MainCharacter.cs
@@ -7,17 +7,25 @@ public class MainCharacter : MonoBehaviour
     [SerializeField]
     GameObject attackRange = default;
 
+    private Rigidbody rigid;
+
     public float moveSpeed = 5.0f;
     Vector3 vecTarget;
+
+    public float jumpPower = 3.0f;
+    bool isJump;
 
     void Start()
     {
         vecTarget = transform.position;
+        rigid = GetComponent<Rigidbody>();
+        isJump = false;
     }
 
     void Update()
     {
         Move();
+        Jump();
         Stop();
         AttackRange();
     }
@@ -42,11 +50,33 @@ public class MainCharacter : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, vecTarget, moveSpeed * Time.deltaTime);
     }
 
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJump)
+            {
+                isJump = true;
+                rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            else
+                return;
+        }
+    }
+
+    void Stop()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveSpeed = 0f;
+        }
+    }
+
     void AttackRange()
     {
+        attackRange.transform.position = transform.position;
         if (Input.GetKeyDown(KeyCode.A))
         {
-            attackRange.transform.position = transform.position;
             attackRange.SetActive(true);
         }
         else if (Input.GetKeyUp(KeyCode.A))
@@ -55,11 +85,11 @@ public class MainCharacter : MonoBehaviour
         }
     }
 
-    void Stop()
+    private void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKeyDown(KeyCode.S))
+        if (collision.gameObject.CompareTag("Floor"))
         {
-            moveSpeed = 0f;
+            isJump = false;
         }
     }
 }
