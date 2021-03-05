06e4686360fe172bb493c6ad52898d748e083cf6
So Joon <joonso16@gmail.com>
2021-03-05 16:18:45 +0900

    turret crush

--- LastProject/Assets/Scripts/In_Game/Bullet.cs
+++ LastProject/Assets/Scripts/In_Game/Bullet.cs
@@ -23,5 +23,9 @@ public class Bullet : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 }
