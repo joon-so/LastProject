5bafd907a97de204db333cce7e67b0004e5c5ac4
Lee Mi Reuk <sslejds@naver.com>
2021-03-15 23:53:26 +0900

    start sub logic

---
+++ LastProject/Assets/Scripts/SubAI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SubAI : MonoBehaviour
+{
+    public enum characterState { idle, trace, attack }
+    public characterState currentState = characterState.idle;
+
+    private NavMeshAgent navMeshAgent;
+    private Transform 
+    private float traceDistance = 10.0f;
+    private float attackDistance = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void CheckEnemy()
+    {
+        
+    }
+}
