aa5d31397245bf23ff328b25227ec01261cbc855
Lee Mi Reuk <sslejds@naver.com>
2021-03-29 22:34:56 +0900

    Update

--- LastProject/Assets/Scripts/Weapon/JadeAssaultRifleBullet.cs
+++ LastProject/Assets/Scripts/Weapon/JadeAssaultRifleBullet.cs
@@ -4,28 +4,194 @@ using UnityEngine;
 
 public class JadeAssaultRifleBullet : MonoBehaviour
 {
-    public int damage;
-    public float range = 11.0f;
-    Vector3 startPoint;
+    public bool rotate = false;
+    public float rotateAmount = 45;
+    public bool bounce = false;
+    public float bounceForce = 10;
+    public float speed;
+    [Tooltip("From 0% to 100%")]
+    public float accuracy;
+    public float fireRate;
+    public GameObject muzzlePrefab;
+    public GameObject hitPrefab;
+    public List<GameObject> trails;
+
+    private Vector3 startPos;
+    private Vector3 offset;
+    private bool collided;
+    private Rigidbody rb;
+    private RotateToMouseScript rotateToMouse;
+    private GameObject target;
 
     void Start()
     {
-        startPoint = transform.position;
-    }
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
 
-    void Update()
-    {
-        if(Vector3.Distance(startPoint, transform.position) > range)
+        //used to create a radius for the accuracy and have a very unique randomness
+        if (accuracy != 100)
         {
-            Destroy(gameObject);
+            accuracy = 1 - (accuracy / 100);
+
+            for (int i = 0; i < 2; i++)
+            {
+                var val = 1 * Random.Range(-accuracy, accuracy);
+                var index = Random.Range(0, 2);
+                if (i == 0)
+                {
+                    if (index == 0)
+                        offset = new Vector3(0, -val, 0);
+                    else
+                        offset = new Vector3(0, val, 0);
+                }
+                else
+                {
+                    if (index == 0)
+                        offset = new Vector3(0, offset.y, -val);
+                    else
+                        offset = new Vector3(0, offset.y, val);
+                }
+            }
+        }
+
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            muzzleVFX.transform.forward = gameObject.transform.forward + offset;
+            var ps = muzzleVFX.GetComponent<ParticleSystem>();
+            if (ps != null)
+                Destroy(muzzleVFX, ps.main.duration);
+            else
+            {
+                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, psChild.main.duration);
+            }
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void FixedUpdate()
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (target != null)
+            rotateToMouse.RotateToMouse(gameObject, target.transform.position);
+        if (rotate)
+            transform.Rotate(0, 0, rotateAmount, Space.Self);
+        if (speed != 0 && rb != null)
+            rb.position += (transform.forward + offset) * (speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision co)
+    {
+        if (!bounce)
         {
-            Destroy(gameObject);
+            if (co.gameObject.tag != "Bullet" && !collided)
+            {
+                collided = true;
+
+                if (trails.Count > 0)
+                {
+                    for (int i = 0; i < trails.Count; i++)
+                    {
+                        trails[i].transform.parent = null;
+                        var ps = trails[i].GetComponent<ParticleSystem>();
+                        if (ps != null)
+                        {
+                            ps.Stop();
+                            Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                        }
+                    }
+                }
+
+                speed = 0;
+                GetComponent<Rigidbody>().isKinematic = true;
+
+                ContactPoint contact = co.contacts[0];
+                Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                Vector3 pos = contact.point;
+
+                if (hitPrefab != null)
+                {
+                    var hitVFX = Instantiate(hitPrefab, pos, rot) as GameObject;
+
+                    var ps = hitVFX.GetComponent<ParticleSystem>();
+                    if (ps == null)
+                    {
+                        var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                        Destroy(hitVFX, psChild.main.duration);
+                    }
+                    else
+                        Destroy(hitVFX, ps.main.duration);
+                }
+
+                StartCoroutine(DestroyParticle(0f));
+            }
+        }
+        else
+        {
+            rb.useGravity = true;
+            rb.drag = 0.5f;
+            ContactPoint contact = co.contacts[0];
+            rb.AddForce(Vector3.Reflect((contact.point - startPos).normalized, contact.normal) * bounceForce, ForceMode.Impulse);
+            Destroy(this);
         }
     }
+
+    public IEnumerator DestroyParticle(float waitTime)
+    {
+
+        if (transform.childCount > 0 && waitTime != 0)
+        {
+            List<Transform> tList = new List<Transform>();
+
+            foreach (Transform t in transform.GetChild(0).transform)
+            {
+                tList.Add(t);
+            }
+
+            while (transform.GetChild(0).localScale.x > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+                transform.GetChild(0).localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+                for (int i = 0; i < tList.Count; i++)
+                {
+                    tList[i].localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+                }
+            }
+        }
+
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+
+    public void SetTarget(GameObject trg, RotateToMouseScript rotateTo)
+    {
+        target = trg;
+        rotateToMouse = rotateTo;
+    }
+
+    //public int damage;
+    //public float range = 11.0f;
+    //public GameObject muzzlePrefab;
+    //public GameObject hitPrefab;
+    //Vector3 startPoint;
+
+    //void Start()
+    //{
+    //    startPoint = transform.position;
+    //}
+
+    //void Update()
+    //{
+    //    if(Vector3.Distance(startPoint, transform.position) > range)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Enemy")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
