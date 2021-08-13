ba7d1898df521d9105e46a3a7230c1327d65e1cf
joon-so <joonso16@gmail.com>
2021-08-14 07:19:23 +0900

    Stage0To1

--- LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Jade0To1Script.cs
+++ LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Jade0To1Script.cs
@@ -8,12 +8,14 @@ public class Jade0To1Script : MonoBehaviour
     Vector3 movePoint2;
     Vector3 movePoint3;
 
+    Rigidbody rigidbody;
 
     Animator anim;
     void Start()
     {
         anim = GetComponent<Animator>();
-        movePoint1 = new Vector3(1.97f, transform.position.y, 20.3f);
+        rigidbody = GetComponent<Rigidbody>();
+        movePoint1 = new Vector3(-13.62f, transform.position.y, -19.11f);
         movePoint2 = new Vector3(1.94f, transform.position.y, 15.15f);
         movePoint3 = new Vector3(6.35f, transform.position.y, 9.48f);
         StartCoroutine(Motion());
@@ -38,5 +40,19 @@ public class Jade0To1Script : MonoBehaviour
         }
         yield return new WaitForSeconds(0.1f);
         anim.SetTrigger("Stretching");
+        yield return new WaitForSeconds(6.35f);
+
+        anim.SetBool("Run", true);
+        rigidbody.freezeRotation = true;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        movePoint1 = new Vector3(-13.62f, transform.position.y, -19.11f);
+
+        transform.LookAt(movePoint1);
+        while (Vector3.Distance(transform.position, movePoint1) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePoint1, 5f * Time.deltaTime);
+            yield return null;
+        }
+        //anim.SetBool("Run", true);
     }
 }
