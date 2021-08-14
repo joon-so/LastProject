474e6179cbfc424cb578641fdbf563064ffdc3c5
joon-so <joonso16@gmail.com>
2021-08-15 02:21:17 +0900

    Stage0To1 Complete

--- LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Karmen0To1Script.cs
+++ LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Karmen0To1Script.cs
@@ -15,8 +15,9 @@ public class Karmen0To1Script : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
 
-        movePoint1 = new Vector3(-13.62f, transform.position.y, -19.11f);
+        movePoint1 = new Vector3(-6.09f, transform.position.y, -17.51f);
         movePoint2 = new Vector3(1.94f, transform.position.y, 15.15f);
         movePoint3 = new Vector3(6.35f, transform.position.y, 9.48f);
         StartCoroutine(Motion());
@@ -24,11 +25,21 @@ public class Karmen0To1Script : MonoBehaviour
 
     IEnumerator Motion()
     {
+        anim.speed = 0f;
+        rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        yield return new WaitForSeconds(5.30f);
+
+        rigidbody.constraints = RigidbodyConstraints.None;
+        Vector3 pos = new Vector3(-31.63f, 9.37f, -20.03f);
+        transform.position = pos;
+        rigidbody.useGravity = true;
+        anim.speed = 1f;
         anim.SetTrigger("Start");
         yield return null;
-        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.95f)
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.95)
         {
-            if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.23f
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.23f
                  && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.10f)
             {
                 anim.speed = 0.31f;
@@ -40,19 +51,18 @@ public class Karmen0To1Script : MonoBehaviour
             yield return null;
         }
         yield return new WaitForSeconds(0.1f);
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         anim.SetTrigger("Stretching");
-        yield return new WaitForSeconds(6.35f);
+        yield return new WaitForSeconds(6f);
 
         anim.SetBool("Run", true);
-        rigidbody.freezeRotation = true;
-        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
-        movePoint1 = new Vector3(-13.62f, transform.position.y, -19.11f);
-
+        movePoint1.y = transform.position.y;
         transform.LookAt(movePoint1);
         while (Vector3.Distance(transform.position, movePoint1) > 0.001f)
         {
-            transform.position = Vector3.MoveTowards(transform.position, movePoint1, 5f * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, movePoint1, 6.5f * Time.deltaTime);
             yield return null;
         }
+        anim.SetBool("Run", false);
     }
 }
--- LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Leina0To1Script.cs
+++ LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Leina0To1Script.cs
@@ -8,12 +8,16 @@ public class Leina0To1Script : MonoBehaviour
     Vector3 movePoint2;
     Vector3 movePoint3;
 
+    Rigidbody rigidbody;
 
     Animator anim;
     void Start()
     {
         anim = GetComponent<Animator>();
-        movePoint1 = new Vector3(1.97f, transform.position.y, 20.3f);
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+
+        movePoint1 = new Vector3(-6.09f, transform.position.y, -17.51f);
         movePoint2 = new Vector3(1.94f, transform.position.y, 15.15f);
         movePoint3 = new Vector3(6.35f, transform.position.y, 9.48f);
         StartCoroutine(Motion());
@@ -21,9 +25,19 @@ public class Leina0To1Script : MonoBehaviour
 
     IEnumerator Motion()
     {
+        anim.speed = 0f;
+        rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        yield return new WaitForSeconds(5.30f);
+
+        rigidbody.constraints = RigidbodyConstraints.None;
+        Vector3 pos = new Vector3(-31.63f, 9.37f, -20.03f);
+        transform.position = pos;
+        rigidbody.useGravity = true;
+        anim.speed = 1f;
         anim.SetTrigger("Start");
         yield return null;
-        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.95f)
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.95)
         {
             if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.23f
                  && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.10f)
@@ -37,6 +51,18 @@ public class Leina0To1Script : MonoBehaviour
             yield return null;
         }
         yield return new WaitForSeconds(0.1f);
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         anim.SetTrigger("Stretching");
+        yield return new WaitForSeconds(5.9f);
+
+        anim.SetBool("Run", true);
+        movePoint1.y = transform.position.y;
+        transform.LookAt(movePoint1);
+        while (Vector3.Distance(transform.position, movePoint1) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePoint1, 6.5f * Time.deltaTime);
+            yield return null;
+        }
+        anim.SetBool("Run", false);
     }
 }
