8de4f3a43e2ba3148161796b6b671ccee6ffed7d
Lee Mi Reuk <sslejds@naver.com>
2021-08-15 14:28:30 +0900

    Boss3Page Update

--- LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Animation/Boss/BossPage2Script.cs
+++ LastProject/Assets/Scripts/Cinemachine/CharacterCinemachineScript/Animation/Boss/BossPage2Script.cs
@@ -21,17 +21,17 @@ public class BossPage2Script : MonoBehaviour
         Instantiate(explosionEffect, transform.position + transform.up * 5.87f + transform.right * 1f, transform.rotation);
         yield return new WaitForSeconds(2.4f);
         backFire.SetActive(true);
-        yield return new WaitForSeconds(1.18f);
-        float flyTime = 1.5f;
-        while(flyTime > 0)
-        {
-            flyTime -= Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward * 1f, 25f * Time.deltaTime);
-            yield return null;
-        }
-        yield return new WaitForSeconds(0.4f);
-        transform.LookAt(transform.position - transform.forward);
-        yield return new WaitForSeconds(0.2f);
+        //yield return new WaitForSeconds(1.18f);
+        yield return new WaitForSeconds(2f);
+        //float flyTime = 1.5f;
+        //while(flyTime > 0)
+        //{
+        //    //flyTime -= Time.deltaTime;
+        //    //transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward * 1f, 25f * Time.deltaTime);
+        //    //yield return null;
+        //}
+        //transform.LookAt(transform.position - transform.forward);
+        yield return new WaitForSeconds(2.5f);
         Frame.SetActive(true);
     }
 
