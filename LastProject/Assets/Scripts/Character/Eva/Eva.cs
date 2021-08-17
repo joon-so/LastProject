7b6a747166a8e8f46e58553ef7c9e245c4c3b6f5
Lee Mi Reuk <sslejds@naver.com>
2021-08-17 10:06:16 +0900

    Udpate

--- LastProject/Assets/Scripts/Character/Eva/Eva.cs
+++ LastProject/Assets/Scripts/Character/Eva/Eva.cs
@@ -720,7 +720,7 @@ public class Eva : SubAI
         }
 
         animator.SetTrigger("WSkill");
-        SoundManager.instance.SFXPlay("EvaEkill", evaESkillSound);
+
         Instantiate(wSkillEffect, wSkillPos.position, wSkillPos.rotation);
         Quaternion rotation = Quaternion.identity;
         rotation.eulerAngles = new Vector3(-90, 0, 0);
@@ -729,7 +729,7 @@ public class Eva : SubAI
         animator.SetFloat("Speed", 0.0f);
         yield return new WaitForSeconds(0.5f);
         animator.SetFloat("Speed", 1.0f);
-
+        SoundManager.instance.SFXPlay("EvaEkill", evaESkillSound);
 
         eSkillHitBox.SetActive(true);
         yield return new WaitForSeconds(1f);
